a53a4a0c033e83324535f7a6bafeeee9dbee0d44
ottoa98@yahoo.com <ottoa98@yahoo.com>
2023-01-31 15:46:12 +0000

    Edited settings, ServerModule1 and custom package dependencies

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -2,16 +2,27 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import anvil.media
+import cv2
 from deepface import DeepFace
 
 
-#@anvil.server.callable
-#def analyze(img):
-  #calling VGGFace
- # model_name = "VGG-Face"
- # model = DeepFace.build_model(model_name)
- # output = DeepFace.analyze(img_path = img, actions = ['age', 'gender', 'race', 'emotion'])
- # return print(obj["age"]," years old ",obj["dominant_race"])
+
+@anvil.server.callable
+def analyze(file):
+  #calling model to use for predictions
+  models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "SFace"]
+  model_name = models[7]
+  model = DeepFace.build_model(model_name)
+
+  #load image file from https://facescanner.anvil.app/
+  with anvil.media.TempFile(file) as filename:
+    img = cv2.imread(filename)
+
+  #use selected model to analyze image. Return output to https://facescanner.anvil.app/
+  obj = DeepFace.analyze(img_path = file, actions = ['age', 'gender', 'race', 'emotion'])
+  print(obj[0]['age'], obj[0]['dominant_gender'], obj[0]['dominant_race'], obj[0]['dominant_emotion'])
+  return obj[0]['age'], obj[0]['dominant_gender'], obj[0]['dominant_race'], obj[0]['dominant_emotion']
 
 
 
