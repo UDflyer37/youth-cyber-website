8c54273b651879a0d0f7b73076e7d7443cd0c47e
ottoa98@yahoo.com <ottoa98@yahoo.com>
2024-09-13 23:20:35 +0000

    Edited ServerModule1 and home.html; Deleted scratch_drawing, face_scanner, cursor.png and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,24 +3,5 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import anvil.media
-#from deepface import DeepFace
-
-
-@anvil.server.callable
-def face(file):
-  #calling model to use for predictions
-  models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "SFace"]
-  model_name = models[7]
-  model = DeepFace.build_model(model_name)
-
-  #load image file from https://facescanner.anvil.app/
-  with anvil.media.TempFile(file) as filename:
-    img = cv2.imread(filename)
-
-  #use selected model to analyze image. Return output to https://facescanner.anvil.app/
-  obj = DeepFace.analyze(img_path = file, actions = ['age', 'gender', 'race', 'emotion'])
-  print(obj[0]['age'], obj[0]['dominant_gender'], obj[0]['dominant_race'], obj[0]['dominant_emotion'])
-  return obj[0]['age'], obj[0]['dominant_gender'], obj[0]['dominant_race'], obj[0]['dominant_emotion']
-
 
 
